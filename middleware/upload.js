efd3c80da8275e616571fc1df07e9a7c0d95172b
agiradaniel <61313029+agiradaniel@users.noreply.github.com>
2022-12-02 23:36:10 +0530

    bug

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,8 +2,8 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        console.log(__dirname.split('\\').slice(0, -1).join('/') + '/uploads/')
-        cb(null, __dirname.split('\\').slice(0, -1).join('/') + '/uploads/');
+        // console.log(__dirname.split('\\').slice(0, -1).join('/') + '/uploads/')
+        cb(null, 'https://ig-clone.onrender.com' + '/uploads/');
     },
     filename: (req, file, cb) => {
         cb(null, new Date().getTime() + '-' + file.originalname);
