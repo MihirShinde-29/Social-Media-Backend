25330c0039f181e46625ae72d28ee7099e9f7881
agiradaniel <61313029+agiradaniel@users.noreply.github.com>
2022-12-02 22:59:57 +0530

    bug#3

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,12 +1,11 @@
 const multer = require("multer");
-const path = require("path");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, __dirname.split('\\').slice(0, -1).join('/') + '/uploads/');
     },
     filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + '-' + file.originalname);
+        cb(null, new Date().getTime() + '-' + file.originalname);
     },
 });
 
