286d040726a9f6b9ded4578bb7dd896bf3152377
agiradaniel <61313029+agiradaniel@users.noreply.github.com>
2022-12-03 00:04:50 +0530

    bug

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 connectDB();
-app.use('./uploads', express.static(path.join(__dirname, "uploads")));
+app.use('uploads', express.static(path.join(__dirname, "uploads")));
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
 app.use("/api/comment", commentRoute);
