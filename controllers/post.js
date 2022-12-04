08844bcc6d361a3ab5c151a233f1c32719f3165a
agiradaniel <61313029+agiradaniel@users.noreply.github.com>
2022-12-04 13:24:06 +0530

    bug#69

--- controllers/post.js
+++ controllers/post.js
@@ -99,7 +99,7 @@ exports.deletePost = async (req, res) => {
       .then(post => {
         if (req.userId === post.user.toString()) {
           if(post.comments.length > 0) {
-            Comment.deleteMany({ user: req.userId })
+            Comment.deleteMany({ user: {username: req.userId} })
               .then(() => {
                 Post.findByIdAndDelete(req.params.id)
                   .then(() => {
